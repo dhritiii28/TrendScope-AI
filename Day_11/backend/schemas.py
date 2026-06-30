690b9aa7ffbfa75bd1224759e7c74edc35f23196
dhritiii28 <dhritibaidwan@gmail.com>
2026-06-30 23:15:04 +0530

    Day 11: Implemented text cleaning and TF-IDF keyword extraction

---
+++ Day_11/backend/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+
+class UserRegister(BaseModel):
+    
+    id: int
+    
+    username: str
+    
+    email: str
+    
+    password: str
+    
+class UserLogin(BaseModel):
+    
+    email: str
+    
+    password: str
+    
+class TrendResponse(BaseModel):
+    
+    title: str
+    
+    description: str
+    
+    source: str
+    
+    published_at: str
+    
+    url: str
+    
+    cleaned_text: str
+
+    class Config:
+        from_attributes = True
+    
+    keywords: str | None = None
